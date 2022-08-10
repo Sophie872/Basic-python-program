05cc996618026d5b46cd145869a28931ef9fc492
Lisbeth Mujica <76069886+Sophie872@users.noreply.github.com>
2022-08-10 17:24:13 -0400

    Create  programa que imprima todos los números pares entre dos números que se le.py

---
+++ programa que imprima todos los números pares entre dos números que se le.py
@@ -0,0 +1,9 @@
+numero1 = int(input("introduce el primer numero par :"))
+numero2 = int(input("introduce el segundo numero par :"))
+start, end = 10, 29
+for num in range(start, end + 1):
+  if num % 2 == 0:
+      print(num, end = " ")
+
+
+#Lisbeth Mujica
