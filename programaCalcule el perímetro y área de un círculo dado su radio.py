48d60bbe89d0980ea677dbee64ce2e5a7258519e
Lisbeth Mujica <76069886+Sophie872@users.noreply.github.com>
2022-08-10 17:31:37 -0400

    Create programaCalcule el perímetro y área de un círculo dado su radio.py

---
+++ programaCalcule el perímetro y área de un círculo dado su radio.py
@@ -0,0 +1,8 @@
+radio = float(input("introduce el radio:"))
+import math 
+perimetro = 2*(math.pi*radio)
+area = (math.pi*radio)**2
+print(f" el area es: {area:.2f}")
+print(f" el perimetro es:{perimetro:.2f}") 
+
+#Lisbeth Mujica
