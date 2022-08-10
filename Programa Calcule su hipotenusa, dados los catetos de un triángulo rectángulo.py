8196f1dcc29ab79214618a3aff4448f60ba542c1
Lisbeth Mujica <76069886+Sophie872@users.noreply.github.com>
2022-08-10 17:35:54 -0400

    Create Programa Calcule su hipotenusa, dados los catetos de un triángulo rectángulo.py

---
+++ Programa Calcule su hipotenusa, dados los catetos de un triángulo rectángulo.py
@@ -0,0 +1,7 @@
+import math
+Cateto1=float (input("Introduce el cateto opuesto:"))
+Cateto2=float (input("Introduce el cateto adyacente:"))
+Hipotenusa =math.sqrt((Cateto1*Cateto1+Cateto2*Cateto2))
+print(f" La Hipotenusa es: {Hipotenusa}")
+
+#Lisbeth Mujica
